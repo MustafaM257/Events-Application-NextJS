fc7d2e91f702da059488a52fccae634e0647ee65
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-21 04:39:39 -0400

    Done with the navbar, next footer

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,12 +1,21 @@
+"use client";
 import { headerLinks } from "@/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavItems = () => {
+  const path = usePathname();
   return (
-    <ul className="flex w-full gap-5 flex-col md:flex-row items-start  ">
+    <ul className="flex w-full gap-5 flex-col md:flex-row items-start">
       {headerLinks.map((link) => {
+        const isActive = path === link.route;
         return (
-          <li>
+          <li
+            key={link.route}
+            className={`${
+              isActive ? "text-primary-500" : ""
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
             <Link href={link.route}>{link.label}</Link>
           </li>
         );
