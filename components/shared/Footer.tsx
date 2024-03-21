57019e938e572b4600209239a7753915297af61b
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-21 04:45:41 -0400

    Done Footer, next to start with content

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,23 @@
+import Logo from "@/public/assets/images/logo.svg";
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src={Logo} alt="Logo Image" width={128} height={38} />
+        </Link>
+        <p className=" font-semibold shadow- shadow-2xl shadow-primary-500 hover:text-primary-500 cursor-pointer">
+          {" "}
+          Developed by Mustafa{" "}
+        </p>
+        <p>{year}. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
