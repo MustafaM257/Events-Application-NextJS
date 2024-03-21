ac465a8226e8eb3e6354887f522c8da8adcd3c58
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-21 03:17:06 -0400

    Add clerk local routes to the project

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/assets/images/logo.svg";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "../ui/button";
 const Navbar = () => {
   return (
     <header className="w-full border-b">
@@ -8,7 +10,13 @@ const Navbar = () => {
         <Link href={"/"} className="w-36">
           <Image src={Logo} alt="Logo Image" width={128} height={38} />
         </Link>
-        <div className="flex w-32 justify-end gap-3"></div>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rouded-full" size={"lg"}>
+              <Link href={"/sign-in"}>Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
