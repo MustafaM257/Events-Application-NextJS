375ba25b38380c8fed5020611307d6370e1b654a
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-21 03:29:51 -0400

    Structure the navbar and work on items and signedin/signedout compoennts

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/assets/images/logo.svg";
-import { SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 const Navbar = () => {
   return (
     <header className="w-full border-b">
@@ -10,7 +12,16 @@ const Navbar = () => {
         <Link href={"/"} className="w-36">
           <Image src={Logo} alt="Logo Image" width={128} height={38} />
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
         <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
           <SignedOut>
             <Button asChild className="rouded-full" size={"lg"}>
               <Link href={"/sign-in"}>Login</Link>
