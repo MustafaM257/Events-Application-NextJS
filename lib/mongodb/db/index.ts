7fad33ed1a678cf904c9ca561e9491cbed6fee4c
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-21 21:44:04 -0400

    Add mongoose & mongodb, establish conxn and work on user model rn

---
+++ lib/mongodb/db/index.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+// The cached variable is used to store the connection and promise to avoid connecting multiple times
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missng");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "events",
+      bufferCommands: false,
+    });
+};
