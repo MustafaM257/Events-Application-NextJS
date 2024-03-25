39424eb6237638179131475d0b22978d4eecb4fb
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-24 23:57:01 -0400

    Finished stat/end date in EventForm, added price & isFree section

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,10 +2,12 @@
 import Image from "next/image";
 import locationImage from "@/public/assets/icons/location-grey.svg";
 import calendarImage from "@/public/assets/icons/calendar.svg";
+import dollarImage from "@/public/assets/icons/dollar.svg";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 import { z } from "zod";
 import {
@@ -170,6 +172,92 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                     <DatePicker
                       selected={field.value}
                       onChange={(date: Date) => field.onChange(date)}
+                      showTimeSelect
+                      timeInputLabel="Time:"
+                      dateFormat={"MMMM d, yyyy h:mm aa"}
+                      wrapperClassName="datePicker"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="endDateTime"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-14 w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                    <Image
+                      src={calendarImage}
+                      alt="Calender icon"
+                      width={25}
+                      height={25}
+                      className="filter-grey"
+                    />
+                    <p className="ml-4 whitespace-nowrap text-grey-600">
+                      End Date:
+                    </p>
+                    <DatePicker
+                      selected={field.value}
+                      onChange={(date: Date) => field.onChange(date)}
+                      showTimeSelect
+                      timeInputLabel="Time:"
+                      dateFormat={"MMMM d, yyyy h:mm aa"}
+                      wrapperClassName="datePicker"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-14 w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                    <Image
+                      src={dollarImage}
+                      alt="Price icon"
+                      width={25}
+                      height={25}
+                      className="filter-grey"
+                    />
+                    <Input
+                      type="number"
+                      placeholder="Price"
+                      {...field}
+                      className="input-field"
+                    />
+                    <FormField
+                      control={form.control}
+                      name="isFree"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="flex items-center ">
+                              <label
+                                htmlFor="isFree"
+                                className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                              >
+                                Free Ticket
+                              </label>
+                              <Checkbox
+                                id="isFree"
+                                className="mr-2 h-5 w-5 border-2 border-primary-500"
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
                   </div>
                 </FormControl>
