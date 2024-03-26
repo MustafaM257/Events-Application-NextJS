e2cc6d6cff03c52295b6a5b23df5aa44d4981569
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-26 01:32:58 -0400

    Start with the event card, implement logic of event , orderlink, and price

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,4 +1,5 @@
 import { IEvent } from "@/lib/mongodb/db/models/event.model";
+import EventCard from "./EventCard";
 
 type CollectionProps = {
   data: IEvent[];
@@ -21,7 +22,34 @@ const Collection = ({
   totalPages,
   urlParamName,
 }: CollectionProps) => {
-  return <div>Collection</div>;
+  return (
+    <div>
+      {data.length > 0 ? (
+        <div className="flex flex-col items-center gap-10">
+          <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2  lg:grid-cols-3 xl:gap-10">
+            {data.map((event) => {
+              const hasOrderLink = collectionType === "Events_Organized";
+              const hidePrice = collectionType === "My_Tickets";
+              return (
+                <li key={event._id} className="flex justify-center">
+                  <EventCard
+                    event={event}
+                    hasOrderLink={hasOrderLink}
+                    hidePrice={hidePrice}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : (
+        <div className="flex-center wrapper min-h-52 flex-col gap-3 rounded-2xl bg-grey-50 py-28 text-center">
+          <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Collection;
