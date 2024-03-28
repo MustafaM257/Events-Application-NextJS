6c893ce2da5a8cafaebf372e3973255b0559d11b
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-27 22:42:19 -0400

    Next to integrate strapi

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,7 +1,18 @@
 import { IEvent } from "@/lib/mongodb/db/models/event.model";
+import { Button } from "../ui/button";
 
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
-  return <div>Checkout</div>;
+  const onCheckout = async () => {
+    console.log("Checkout");
+  };
+  return (
+    <form action={onCheckout} method="POST">
+      {/* Stripe functionality here */}
+      <Button type="submit" role="link" size={"lg"} className="button sm:w-fit">
+        {event.isFree ? "Get Free Ticket" : "Buy Ticket"}
+      </Button>
+    </form>
+  );
 };
 
 export default Checkout;
