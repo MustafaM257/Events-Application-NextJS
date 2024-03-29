bee5f5aa35059d7abe555615f3a4c96b95c70b0f
Mustafa Muhammad <mustafa.m257@proton.me>
2024-03-28 23:22:47 -0400

    Add stripe integration from docs

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,10 +1,29 @@
 import { IEvent } from "@/lib/mongodb/db/models/event.model";
 import { Button } from "../ui/button";
-
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect } from "react";
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
   const onCheckout = async () => {
     console.log("Checkout");
   };
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
   return (
     <form action={onCheckout} method="POST">
       {/* Stripe functionality here */}
